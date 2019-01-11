cbbfdd07abb49e4b4d69e91c29137c80c95bede0
hotrunganh1912 <hotrunganhht1912@gmail.com>
2019-01-11 10:32:36 +0700

    chapter12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by(id: params[:id])
-    return if @user
-    flash[:danger] = t ".message_not_found"
-    redirect_to root_path
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -13,17 +17,63 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = t ".message"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t(".message_gmail")
+      redirect_to root_url
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = t(".profile_updated")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t(".user_deleted")
+    redirect_to users_url
+  end
+
   private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".message_not_found"
+    redirect_to root_path
+  end
+
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t(".please_log_in")
+    redirect_to login_url
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    # @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
